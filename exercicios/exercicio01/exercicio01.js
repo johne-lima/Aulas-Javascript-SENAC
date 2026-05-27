970733f5bf4c4f90a9c6cabf68f08b6e2b089a34
Johne Lima <jk.lima014@gmail.com>
2026-05-27 11:50:31 -0300

    atualizado

--- exercicios/exercicio01/exercicio01.js
+++ exercicios/exercicio01/exercicio01.js
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 01 (corrigido)</title>
-</head>
-<body>
-<h1>Exercício 01 (corrigido)</h1>
-    
-<script>
 // Etapa A
 let numero1 = 1000;
 let numero2 = 100;
@@ -41,9 +30,3 @@ let total = soma + subtracao + multiplicacao + divisao;
 let media = total / 4;
 
 console.log(media);
-
-
-</script>
-    
-</body>
-</html>
